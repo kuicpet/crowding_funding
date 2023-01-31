c0d1e01b303f4058276aa34bfc4bdf2338667b96
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-01-31 23:26:46 +0100

    ft--client added

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
+
+// This is the chainId your dApp will work on.
+// const activeChainId = ChainId.Mainnet; Ethereum Mainnet
+const activeChainId = ChainId.Goerli;  // Goerli Testnet
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
