94a1d62c2283b927932277b4bf90ac2de02395fa
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-02-02 21:46:28 +0100

    ft--updated client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,22 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
+import './styles/globals.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // This is the chainId your dApp will work on.
 // const activeChainId = ChainId.Mainnet; Ethereum Mainnet
-const activeChainId = ChainId.Goerli;  // Goerli Testnet
+const activeChainId = ChainId.Goerli // Goerli Testnet
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const container = document.getElementById('root')
+const root = createRoot(container)
 root.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={activeChainId}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ThirdwebProvider>
   </React.StrictMode>
-);
+)
